5ec3240edf426fe00258095506302d63f238c385
AliZhakezhanov1 <ali@midasanalytics.ai>
2022-09-04 00:56:21 +0600

    feat: update:crypto & create:crypto(mock)

--- src/shared/api/cryptos/cryptos.ts
+++ src/shared/api/cryptos/cryptos.ts
@@ -1,11 +1,18 @@
-import { instance } from './base'
+// import { instance } from './base'
+import { getCryptosMock } from './mock'
 import { ICryptoParam, ICryptos } from './types'
 
-export const getCryptos = async ({ page }: ICryptoParam): Promise<ICryptos> => {
-	const res = await instance({
-		method: 'GET',
-		url: `/cryptos/?page=${page}`
-	})
+export const getCryptos = async ({ page = 1 }: ICryptoParam): Promise<ICryptos[]> => {
+	/* use with backend */
+
+	// const res = await instance({
+	// 	method: 'GET',
+	// 	url: `/cryptos/?page=${page}`
+	// })
+
+	/* can comment or delete */
+	type Res = ReturnType<typeof getCryptosMock>
+	const res: Awaited<Res> = await getCryptosMock()
 
 	return res.data
 }
--- src/shared/api/cryptos/types.ts
+++ src/shared/api/cryptos/types.ts
@@ -48,5 +48,5 @@ export interface ICryptoVolTransparency {
 }
 
 export interface ICryptoParam {
-	page: number
+	page?: number
 }
